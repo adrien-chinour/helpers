c1e8a68a7d1f701ecfab6bbbfb39279b0b569b35
Adrien Chinour <git@chinour.fr>
2022-05-06 02:03:22 +0200

    feat(parsedown): Add parsedown endpoint

---
+++ src/Api/ParsedownEndpoint.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Api;
+
+use Parsedown;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ParsedownEndpoint
+{
+    public function __invoke(Request $request): Response
+    {
+        $parser = new Parsedown();
+        return new Response($parser->parse($request->request->get('text')));
+    }
+}
