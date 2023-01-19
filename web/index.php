f9fcc2390c940db0e40ab3e3257104795c3e482b
Adrien Chinour <git@chinour.fr>
2023-01-19 22:49:24 +0100

    chore(whoops): Adding Whoops package to handle exceptions

--- web/index.php
+++ web/index.php
@@ -12,12 +12,22 @@ $endpoints = [
     '/parsedown' => new ParsedownEndpoint(),
 ];
 
+// Initialize Whoops exception handler
+$exceptionHandler = null;
+if (class_exists(Whoops\Run::class)) {
+    $exceptionHandler = new \Whoops\Run;
+    $exceptionHandler->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $exceptionHandler->register();
+}
+
 try {
     $request = Request::createFromGlobals();
     $endpoint = $endpoints[$request->getPathInfo()];
     $response = $endpoint !== null ? $endpoint($request) : new Response("<h1>404 not found :/</h1>", 404);
     $response->headers->set('Access-Control-Allow-Origin', ['https://umami.chinour.dev', 'https://ackee.chinour.dev']);
     $response->prepare($request)->send();
-} catch (Throwable $e) {
-    echo $e->getMessage();
+} catch (Throwable $exception) {
+    $exceptionHandler?->handleException($exception);
+    (new Response("<h1>C cassé :(</h1>", 500))->prepare($request)->send();
 }
+
